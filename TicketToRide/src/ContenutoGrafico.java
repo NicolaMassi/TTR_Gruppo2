4ce98ad2bb39e00d15ba6f5e278c4912f5d90c78
davidlore2004 <david.lore@isitgo.it>
2022-12-14 10:08:20 +0100

    Pannello

---
+++ TicketToRide/src/ContenutoGrafico.java
@@ -0,0 +1,84 @@
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+
+public class ContenutoGrafico extends JPanel{
+
+
+    private Timer timer;
+    private final int DELAY = 10;
+
+    public ContenutoGrafico() {
+
+        initBoard();
+    }
+
+    private void initBoard() {
+
+        addKeyListener(new TAdapter());
+        setBackground(Color.black);
+	setFocusable(true);
+
+        spaceShip = new SpaceShip();
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        doDrawing(g);
+        
+        Toolkit.getDefaultToolkit().sync();
+    }
+    
+    private void doDrawing(Graphics g) {
+        
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.drawImage(spaceShip.getImage(), spaceShip.getX(), 
+            spaceShip.getY(), this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+        step();
+    }
+    
+    private void step() {
+        
+        spaceShip.move();
+        
+        repaint(spaceShip.getX()-1, spaceShip.getY()-1, 
+                spaceShip.getWidth()+2, spaceShip.getHeight()+2);     
+    }    
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            spaceShip.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            spaceShip.keyPressed(e);
+        }
+    }
+}
+    
+
